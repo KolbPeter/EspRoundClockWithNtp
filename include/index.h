dae3121f67a5cb8dfc3d4aa7000d538dd1414c5b
KolbPeter <mr.kolb.peter@gmail.com>
2022-09-28 15:47:12 +0200

    initial commit

---
+++ include/index.h
@@ -0,0 +1,134 @@
+const char MAIN_page[] PROGMEM = R"=====(
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<script>
+		
+		setInterval(function() {
+			callEspFunction('getStats');
+		}, 10000); //10.000 mSeconds update rate
+		
+		function callEspFunction(funcToCall) {
+		 	var xhttp = new XMLHttpRequest();
+		  	xhttp.onreadystatechange = function() {
+		   	if (this.readyState == 4 && this.status == 200) {
+		   		var datas = this.responseText.split(";");
+		      	document.getElementById("Temp").innerHTML = datas[0];
+		      	document.getElementById("Pres").innerHTML = datas[1];
+		    	}
+		  	};
+		  	xhttp.open("GET", funcToCall, false);
+		  	xhttp.send();
+		}
+	</script>
+</head>
+<style>
+	body{
+	    background: #020011;
+	}
+	
+	.card{
+	    background: #07007E;
+	    width: 400px;
+	    padding: 10px;
+	    border-radius: 15px;
+	    box-sizing: border-box;
+	    color: #FFF;
+	    margin:10px;
+	    box-shadow: 0px 2px 18px -1px rgba(0,0,0,0.75);
+	}
+	
+	h2,h1{
+		line-height: 1;
+		margin: 0px;	
+	}
+	
+	button, input {
+  		border: none;
+  		width: 150px;
+  		height: 50px;
+  		color: white;
+  		text-align: center;
+  		margin: 3px 1px;
+  		border-radius: 10px;
+  		cursor: pointer;
+  		outline: none;
+	}
+	
+	.on{
+  		background-color: darkgreen;	
+	}
+	
+	.on:hover{
+		background-color: green;	
+	}
+	
+	.off{
+  		background-color: darkred;		
+	}
+	
+	.off:hover{
+		background-color: red;	
+	}
+	
+	.other{
+  		background-color: darkgoldenrod;		
+	}
+	
+	.other:hover{
+		background-color: goldenrod;	
+	}
+	
+	fieldset{
+		border: 3px solid white;
+	}
+	
+	hr.rounded {
+		border-top: 3px solid white;
+		border-radius: 3px;
+	}
+</style>
+<body onload="callEspFunction('getStats')" id="body">
+  	<fieldset class="card" style="width: 90%">
+
+  	<legend><h1>Clock set</h1></legend>
+
+		 <form action="/get">
+			Set hours: <input class="card" type="number" id="hours" name="hours" min="0" max="23">
+			<input class="on" type="submit" value="Submit">
+		</form>
+
+		<hr class="rounded">
+
+		 <form action="/get">
+			Set minutes: <input class="card" type="number" id="mins" name="mins" min="0" max="59">
+			<input class="on" type="submit" value="Submit">
+		</form>
+
+		<hr class="rounded">
+
+		 <form action="/get">
+			Set seconds: <input class="card" type="number" id="secs" name="secs" min="0" max="59">
+			<input class="on" type="submit" value="Submit">
+		</form>
+
+		<hr class="rounded">
+
+		 <form action="/get">
+			Set light: <input class="card" type="number" id="light" name="light" min="0" max="255">
+			<input class="on" type="submit" value="Submit">
+		</form>
+
+	</fieldset>
+
+	<fieldset class="card">
+
+		<legend><h1>BMP sensor</h1></legend>
+	  	<h2>Temperature : <span id="Temp">0</span> C°</h2>
+	  	<h2>Air pressure : <span id="Pres">0</span> hPa</h2>
+
+	</fieldset>
+</body>
+</html>
+)=====";
---
+++ src/main.cpp
@@ -0,0 +1,158 @@
+#include <WiFi.h>
+#include <WebServer.h>
+#include <AutoConnect.h>
+#include <Adafruit_BMP280.h>
+
+#include <Arduino.h>
+#include <NeoPixelBus.h>
+#include <Wire.h>
+#include <TimeLib.h>
+
+#include "index.h"
+
+#define BMP_SCK (15)
+#define BMP_MISO (16)
+#define BMP_MOSI (2)
+#define BMP_CS (4)
+
+#define LED_COUNT 60
+#define LED_PIN 22
+#define BASE_YEAR 2000
+String PARAM_INPUT_1 = "hours";
+String PARAM_INPUT_2 = "mins";
+String PARAM_INPUT_3 = "secs";
+String PARAM_INPUT_4 = "light";
+int maxLight = 150;
+
+void rootPage();
+String collect_Info();
+void getStats();
+void getRequestHandler();
+void setLeds();
+void setLeds(int red, int green, int blue);
+
+void setup();
+void loop();
+
+WebServer Server;
+AutoConnect Portal(Server);
+Adafruit_BMP280 sensor(BMP_CS, BMP_MOSI, BMP_MISO, BMP_SCK);
+int altitude = 235;
+const float ALTMOD = 11.25; //11,25Pa/méter hozzávetőlegesen 1000 méterig
+
+time_t last = second(now());
+NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> strip(LED_COUNT, LED_PIN);
+RgbColor color(0, 0, 0);
+
+void rootPage()
+{
+  String s = MAIN_page;
+  Server.send(200, "text/html", s);
+}
+
+String collect_Info(){
+  String retval = (String)sensor.readTemperature()+";";
+  retval += (String)((sensor.readPressure() + (ALTMOD * (float)altitude)) / 100)+";";
+  return retval;
+}
+
+void getStats()
+{
+  Server.send(200, "text/plane", collect_Info());
+}
+
+void getRequestHandler()
+{
+  if (Server.hasArg(PARAM_INPUT_1) == true){
+    setTime(
+      Server.arg(PARAM_INPUT_1).toInt(),
+      minute(now()),
+      second(now()),
+      1,1,1);
+  }
+  if(Server.hasArg(PARAM_INPUT_2) == true){
+    setTime(
+      hour(now()),
+      Server.arg(PARAM_INPUT_2).toInt(),
+      second(now()),
+      1,1,1);
+  }
+  if(Server.hasArg(PARAM_INPUT_3) == true){
+    setTime(
+      hour(now()),
+      minute(now()),
+      Server.arg(PARAM_INPUT_3).toInt(),
+      1,1,1);
+  }
+  if(Server.hasArg(PARAM_INPUT_4) == true){
+    maxLight = Server.arg(PARAM_INPUT_4).toInt();
+  }
+  if(
+    Server.hasArg(PARAM_INPUT_1) == false
+    && Server.hasArg(PARAM_INPUT_2) == false
+    && Server.hasArg(PARAM_INPUT_3) == false
+    && Server.hasArg(PARAM_INPUT_4) == false){
+      Server.send(200, "text/plain", "No data received");
+    return;
+  }
+
+  setLeds();
+  rootPage();
+}
+
+void setLeds(){
+  setLeds(hour(now()),minute(now()),second(now()));
+}
+
+void setLeds(int hour, int min, int sec){
+  int roundClockHour = hour;
+  if (roundClockHour >= 12){ roundClockHour = roundClockHour - 12; };
+  roundClockHour = roundClockHour * 5;
+  roundClockHour = roundClockHour + (minute(now()) / 12);
+
+  for (int i=0;i<LED_COUNT;i++){
+    if (i==roundClockHour){ color.R = maxLight; }else{ color.R = 0; };
+    if (i==min){ color.G = maxLight; }else{ color.G = 0; };
+    if (i==sec){ color.B = maxLight; }else{ color.B = 0; };
+    strip.SetPixelColor(i, color);
+  }
+  strip.Show();
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  delay(100);
+  sensor.begin();
+  sensor.setSampling(
+      Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+      Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+      Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+      Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+      Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+  delay(1000);
+  Serial.println("Adafruit connected!");
+  if (Portal.begin())
+  {
+    Serial.println("HTTP server:" + WiFi.localIP().toString());
+    char __SSID[WiFi.SSID().length() + 1];
+    char __PASS[WiFi.psk().length() + 1];
+    (WiFi.SSID()).toCharArray(__SSID, sizeof(__SSID));
+    (WiFi.psk()).toCharArray(__PASS, sizeof(__PASS));
+  }
+  Server.on("/", rootPage);
+  Server.on("/getStats", getStats);
+  Server.on("/get", HTTP_GET, getRequestHandler);
+
+  strip.Begin();
+  setTime(0,0,0,1,1,1);
+}
+
+void loop()
+{
+  Portal.handleClient();
+  if (second(now())!=last){
+    setLeds(hour(now()),minute(now()),second(now()));
+    last=second(now());
+  }
+}
